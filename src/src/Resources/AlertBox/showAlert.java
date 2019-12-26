5a6228efeb565c07b2fe48ebefe0a2940c444303
blackdracula18 <Abubakaryusuf951@gmail.com>
2019-12-26 20:17:52 +0800

    kinda finish

---
+++ src/src/Resources/AlertBox/showAlert.java
@@ -0,0 +1,30 @@
+package Resources.AlertBox;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Window;
+
+public class showAlert extends GridPane {
+
+    public showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        String css = getClass().getResource("Alert.css").toExternalForm();
+        this.getStylesheets().add(css);
+        System.out.println(this);System.out.println(this);System.out.println(this);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+    public showAlert(Window owner, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        String css = getClass().getResource("Alert.css").toExternalForm();
+        this.getStylesheets().add(css);
+        System.out.println(this);System.out.println(this);
+        alert.setTitle("Form Error!");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+}
